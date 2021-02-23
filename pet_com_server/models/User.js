1d1f05b86db379f3dea72f53cdcbbc903b987df2
请不要阻拦我学习 <924370430@qq.com>
2021-02-23 12:02:55 +0800

    tt

--- pet_com_server/models/User.js
+++ pet_com_server/models/User.js
@@ -5,9 +5,10 @@ const schema = new mongoose.Schema({
   follow: [{type: mongoose.SchemaTypes.ObjectId, ref: 'User'}],
   fans: [{type: mongoose.SchemaTypes.ObjectId, ref: 'User'}],
   password: {
+    type: String,
     select: false,
     set(val) {
-      return require('bcrypt').hashSync(val.toString(), 10)
+      return require('bcrypt').hashSync(val, 10)
     }
   },
   avatar: {type: String}
--- pet_com_server/routes/web/index.js
+++ pet_com_server/routes/web/index.js
@@ -71,6 +71,7 @@ module.exports = app => {
     // 先寻找用户
     const user = await User.findOne({username: req.body.username})
     assert(!user, 422, '用户名已存在')
+    console.log('密码', req.body.password, typeof req.body.password)
     const model = await User.create(req.body)
     res.send(model)
   })

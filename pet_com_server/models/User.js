a78ccfe1ee2a44ff3fb59a41d81e2a9266a5d4d1
请不要阻拦我学习 <924370430@qq.com>
2021-02-23 11:55:42 +0800

    test

--- pet_com_server/models/User.js
+++ pet_com_server/models/User.js
@@ -5,10 +5,9 @@ const schema = new mongoose.Schema({
   follow: [{type: mongoose.SchemaTypes.ObjectId, ref: 'User'}],
   fans: [{type: mongoose.SchemaTypes.ObjectId, ref: 'User'}],
   password: {
-    type: String,
     select: false,
     set(val) {
-      return require('bcrypt').hashSync(val, 10)
+      return require('bcrypt').hashSync(val.toString(), 10)
     }
   },
   avatar: {type: String}
